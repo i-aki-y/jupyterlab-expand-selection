56fd5232dd7f88dd1a8843c0dbaafe04150ea1aa
akiyuki ishikawa <aki.y.ishikawa@gmail.com>
2025-11-28 10:19:41 +0900

    add codemirror-expand-selection extension and related commands

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,100 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { INotebookTracker } from '@jupyterlab/notebook';
+import {
+  CodeMirrorEditor,
+  IEditorExtensionRegistry,
+  EditorExtensionRegistry
+} from '@jupyterlab/codemirror';
+import { EditorView } from '@codemirror/view';
+import {
+  expandSelectionExtension,
+  expandSelection,
+  shrinkSelection,
+  swapAnchorHead
+} from 'codemirror-expand-selection';
 
 /**
  * Initialization data for the jupyterlab-expand-selection extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-expand-selection:plugin',
-  description: 'A JupyterLab extension that introduces expand/shrink selection commands',
+  description:
+    'A JupyterLab extension that introduces expand/shrink selection commands',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab-expand-selection is activated!');
+  requires: [INotebookTracker, IEditorExtensionRegistry],
+  activate: (
+    app: JupyterFrontEnd,
+    notebookTracker: INotebookTracker,
+    registry: IEditorExtensionRegistry
+  ) => {
+    console.log(
+      'JupyterLab extension jupyterlab-expand-selection is activated!'
+    );
+    registry.addExtension(
+      Object.freeze({
+        name: 'jupyterlab-expand-selection:expand-selection-extension',
+        factory: () =>
+          EditorExtensionRegistry.createConfigurableExtension(
+            () => expandSelectionExtension
+          )
+      })
+    );
+    app.commands.addCommand('jupyterlab-expand-selection:expand-selection', {
+      label: 'Expand selected region',
+      execute: () => {
+        const notebook = notebookTracker.currentWidget;
+        if (!notebook) {
+          console.warn('No active notebook');
+          return;
+        }
+        const activeCell = notebook.content.activeCell;
+        if (!activeCell) {
+          console.log(activeCell);
+          return;
+        }
+        const cmEditor = activeCell.editor as CodeMirrorEditor;
+        const view = cmEditor.editor as EditorView;
+        expandSelection(view);
+      }
+    });
+
+    app.commands.addCommand('jupyterlab-expand-selection:shrink-selection', {
+      label: 'Shrink selected region',
+      execute: () => {
+        const notebook = notebookTracker.currentWidget;
+        if (!notebook) {
+          console.warn('No active notebook');
+          return;
+        }
+        const activeCell = notebook.content.activeCell;
+        if (!activeCell) {
+          return;
+        }
+        const cmEditor = activeCell.editor as CodeMirrorEditor;
+        const view = cmEditor.editor as EditorView;
+        shrinkSelection(view);
+      }
+    });
+
+    app.commands.addCommand('jupyterlab-expand-selection:swap-anchor-head', {
+      label: 'Swap anchor and head of the selected region',
+      execute: () => {
+        const notebook = notebookTracker.currentWidget;
+        if (!notebook) {
+          console.warn('No active notebook');
+          return;
+        }
+        const activeCell = notebook.content.activeCell;
+        if (!activeCell) {
+          return;
+        }
+        const cmEditor = activeCell.editor as CodeMirrorEditor;
+        const view = cmEditor.editor as EditorView;
+        swapAnchorHead(view);
+      }
+    });
   }
 };
 
